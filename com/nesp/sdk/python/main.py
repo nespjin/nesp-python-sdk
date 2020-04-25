3a6e7a9e382df582a76960d9cf59a52344d9962f
靳兆鲁 <1756404649@qq.com>
2020-04-25 22:43:42 +0800

    format python file header

--- com/nesp/sdk/python/main.py
+++ com/nesp/sdk/python/main.py
@@ -1,5 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Team: NESP Technology
+Author: <a href="mailto:1756404649@qq.com">靳兆鲁 Email:1756404649@qq.com</a>
+Time: Created 2020/4/25 22:42
+Project: NespPythonSdk
+Description:
+"""
+
+
 def main():
     pass
+
 
 if __name__ == "__main__":
     main()
